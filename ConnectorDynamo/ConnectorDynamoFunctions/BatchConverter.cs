fda542f5f59eded210e3d99707ed69c682b18fb9
Jenessa Man <jenessa.man@arup.com>
2022-06-24 19:08:11 -0400

    Add missing using directive

--- ConnectorDynamo/ConnectorDynamoFunctions/BatchConverter.cs
+++ ConnectorDynamo/ConnectorDynamoFunctions/BatchConverter.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Speckle.Core.Logging;
+using System.Text.RegularExpressions;
 
 namespace Speckle.ConnectorDynamo.Functions
 {
