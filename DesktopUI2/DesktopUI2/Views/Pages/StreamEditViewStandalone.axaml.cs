56353941c596df1155f0dde688e8d7ba9951545a
Daniel Naughton <77862637+dtnaughton@users.noreply.github.com>
2023-05-24 13:41:15 -0400

    fix log control on standalone

--- DesktopUI2/DesktopUI2/Views/Pages/StreamEditViewStandalone.axaml.cs
+++ DesktopUI2/DesktopUI2/Views/Pages/StreamEditViewStandalone.axaml.cs
@@ -1,15 +1,30 @@
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using DesktopUI2.ViewModels;
+using ReactiveUI;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Windows.Input;
 
 namespace DesktopUI2.Views.Pages
 {
   public partial class StreamEditViewStandalone : ReactiveUserControl<StreamViewModel>
   {
+    public ItemsRepeater ResultLog => this.FindControl<ItemsRepeater>("ReportLog");
     public StreamEditViewStandalone()
     {
       InitializeComponent();
       Instance = this;
+
+      this.WhenActivated(disposables =>
+      {
+        var updateReportCmd = new UpdateReportCommand(this.ViewModel, ResultLog);
+
+        this.WhenAnyValue(x => x.ViewModel.Progress.IsProgressing).InvokeCommand(updateReportCmd).DisposeWith(disposables);
+      });
     }
 
     private void InitializeComponent()
@@ -17,8 +32,31 @@ namespace DesktopUI2.Views.Pages
       AvaloniaXamlLoader.Load(this);
 
     }
-
     public static StreamEditViewStandalone Instance { get; private set; }
 
+  }
+
+  internal class UpdateReportCommand : ICommand
+  {
+    private readonly StreamViewModel _streamViewModel;
+    private readonly ItemsRepeater _itemsControl;
+
+    public UpdateReportCommand(StreamViewModel streamViewModel, ItemsRepeater itemsControl)
+    {
+      _streamViewModel = streamViewModel;
+      _itemsControl = itemsControl;
+    }
+
+    public event System.EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+      return _streamViewModel != null && !string.IsNullOrEmpty(_streamViewModel.Log);
+    }
+
+    public void Execute(object parameter)
+    {
+      _itemsControl.Items = _streamViewModel.Log.Split('\n');
+    }
   }
 }
