b8491df29f4e93f5372bc583cdbb0f21ad101249
Daniel Naughton <Daniel.Naughton@arup.com>
2023-07-20 16:35:24 -0400

    fix text box setting type

--- DesktopUI2/DesktopUI2/Models/Settings/TextBoxSetting.cs
+++ DesktopUI2/DesktopUI2/Models/Settings/TextBoxSetting.cs
@@ -5,12 +5,12 @@ namespace DesktopUI2.Models.Settings;
 
 public class TextBoxSetting : ISetting
 {
-  public string Type => typeof(CheckBoxSetting).ToString();
+  public string Type => typeof(TextBoxSetting).ToString();
   public string Name { get; set; }
   public string Slug { get; set; }
   public string Icon { get; set; }
   public string Description { get; set; }
   public string Selection { get; set; }
-  public Type ViewType { get; } = typeof(CheckBoxSettingView);
+  public Type ViewType { get; } = typeof(TextBoxSettingView);
   public string Summary { get; set; }
 }
