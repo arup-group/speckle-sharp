41a07c72aa43aadfc3abd67ea8fd1138b1dc5072
BovineOx <73857041+BovineOx@users.noreply.github.com>
2023-11-13 14:53:31 +0000

    Hotfix to tolerate server branch limits of 100 and 500 (#3039)
    Hooked StreamGetBranches for retry if 500 limit fails
    
    We are catching the SpeckleGraphQLException<StreamData> exception as a way to see that the call to get up to 500 stream branches failed. We can then retry with 100.
    
    While the exception isn't as specific as we would like, it is probably good enough and we don't have anything else to go on, based on how the GQL library is constructed.

--- DesktopUI2/DesktopUI2/ViewModels/StreamViewModel.cs
+++ DesktopUI2/DesktopUI2/ViewModels/StreamViewModel.cs
@@ -23,6 +23,7 @@ using DesktopUI2.Views;
 using DesktopUI2.Views.Pages;
 using DesktopUI2.Views.Windows.Dialogs;
 using DynamicData;
+using GraphQL;
 using Material.Dialog.Icons;
 using Material.Icons;
 using Material.Icons.Avalonia;
@@ -252,7 +253,7 @@ public class StreamViewModel : ReactiveObject, IRoutableViewModel, IDisposable
       AvailableFilters = new List<FilterViewModel>(Bindings.GetSelectionFilters().Select(x => new FilterViewModel(x)));
       SelectedFilter = AvailableFilters[0];
 
-      Branches = await Client.StreamGetBranches(Stream.id, ServerLimits.BRANCH_GET_LIMIT, 0).ConfigureAwait(true);
+      Branches = await Client.StreamGetBranchesWithLimitRetry(Stream.id, 0).ConfigureAwait(true);
 
       var index = Branches.FindIndex(x => x.name == StreamState.BranchName);
       if (index != -1)
@@ -420,7 +421,7 @@ public class StreamViewModel : ReactiveObject, IRoutableViewModel, IDisposable
     try
     {
       var prevBranchName = SelectedBranch != null ? SelectedBranch.Branch.name : StreamState.BranchName;
-      Branches = await Client.StreamGetBranches(Stream.id, ServerLimits.BRANCH_GET_LIMIT, 0).ConfigureAwait(true);
+      Branches = await Client.StreamGetBranchesWithLimitRetry(Stream.id, 0).ConfigureAwait(true);
 
       var index = Branches.FindIndex(x => x.name == prevBranchName);
       if (index != -1)
